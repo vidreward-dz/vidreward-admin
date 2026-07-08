3d4070d103116811207e268bcfc89a771604154f
vidreward-dz <slinemidou@gmail.com>
2026-07-08 16:56:27 +0200

    Update Videos.jsx

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -349,7 +349,7 @@ function UploadModal({ campaigns, onClose, onUploaded, onError, onAuthError }) {
         fileName: file.name,
         fileType: file.type || "video/mp4",
       });
-      alert("uploadUrl");
+      alert(uploadUrl);
       
       console.log("uploadUrl:", uploadUrl);
       console.log("publicUrl:", publicUrl); 
