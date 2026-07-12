92baa13223f8d5304067a516c594671f1e7a12f4
vidreward-dz <slinemidou@gmail.com>
2026-07-13 00:35:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
     <BrowserRouter>
       <AuthProvider> 
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} /> 
           <Route 
             path="/"
             element={
